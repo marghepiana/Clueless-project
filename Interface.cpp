0c2f86ffc6c8433c1628efd53f983dd88ca1c406
ruizmar1 <114714808+ruizmar1@users.noreply.github.com>
2022-12-10 18:55:06 -0500

    Create Interface.cpp
    initial commit

---
+++ Interface.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string headwear, top, jacket, bottom, footwear;
+    cout<<endl;
+    cout<<"****************"<<endl;
+    cout<<"**Ugh. As if!!**"<<endl;
+    cout<<"****************"<<endl;
+    cout<<endl;
+    cout<<"Please enter file name of...."<<endl;
+    cout<<"(If none, please write N/A)"<<endl<<endl;
+    cout<<"Headwear: ";
+    cin>>headwear;
+    cout<<"Top: ";
+    cin>>top;
+    cout<<"Jacket: ";
+    cin>>jacket;
+    cout<<"Bottom: ";
+    cin>>bottom;
+    cout<<"Footwear: ";
+    cin>>footwear;
+    cout<<endl;
+    cout<<endl;
+    //some way to maybe output the photos??
+    //is there any way we can check to see if they even imputted clothes?? 
+    //also, function needs to be able to take care of 'N/A' inputs. 
+    cout<<"Calculating compatibility of outfit based on colors and textures...."<<endl;
+    //function call here
+
+
+    return 0;
+
+}   
