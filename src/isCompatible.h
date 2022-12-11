be58456e005b76d5e65c2d6b1556fd9635a0d370
Tara Gill <114505427+taragill25@users.noreply.github.com>
2022-12-11 17:40:36 -0500

    Add files via upload

---
+++ src/isCompatible.h
@@ -0,0 +1,9 @@
+#ifndef ISCOMPATIBLE_H
+#define ISCOMPATIBLE_H
+
+//so far only checks if someone's shirt and pant colors are compatible
+//would work with head, foot and jacket if given more time
+//(5 conditions with 11 diff colors is so many options!!)
+bool isCompatible(Clothing, Clothing);
+
+#endif
