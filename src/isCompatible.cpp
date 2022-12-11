27246598d7a6b21eba4bce20bc53168367a393f1
Tara Gill <114505427+taragill25@users.noreply.github.com>
2022-12-11 17:40:56 -0500

    Add files via upload

---
+++ src/isCompatible.cpp
@@ -0,0 +1,28 @@
+#include "isCompatible.h"
+#include "Clothing.h"
+using namespace std;
+
+bool isCompatible(Clothing top, Clothing bottom){
+	if (top.getColor() == bottom.getColor)
+		return true;
+	else if (top.getColor() == BLACK || bottom.getColor() == BLACK)
+		return true;
+	else if (top.getColor() == WHITE || bottom.getColor() == WHITE)
+		return true;
+	else if (top.getColor() == GRAY || bottom.getColor() == GRAY)
+		return true;
+	else if (bottom.getColor() == BLUE)
+		return true
+	else if (top.getColor() == PINK && bottom.getColor() == GREEN)
+		return true;
+	else if (top.getColor() == GREEN && bottom.getColor() == YELLOW)
+		return true;
+	else if (top.getColor() == RED && bottom.getColor() == YELLOW)
+		return true;
+	else if (top.getColor() == PURPLE && bottom.getColor() == CYAN)
+		return true;
+	else if (top.getColor() == RED && bottom.getColor() == PINK)
+		return true;
+	else 
+		return false;
+}
