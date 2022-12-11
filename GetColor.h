a6cadeedbfc00296d37fc93a22e83dccf32de36d
Tara Gill <114505427+taragill25@users.noreply.github.com>
2022-12-11 15:35:55 -0500

    Add files via upload

---
+++ GetColor.h
@@ -0,0 +1,11 @@
+#include "Clothing.h"
+using namespace std;
+
+#ifndef GET_COLOR
+#define GET_COLOR
+
+//based on RGB values, says which color it is on color wheel
+//mostly just basic colors, if it can't fit into a basic color it is ugly
+int GetColor();
+
+#endif
