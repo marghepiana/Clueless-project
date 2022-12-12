78d86000278f24efd37f91b6e6830f1acca871c5
suhanimitra <113700563+suhanimitra@users.noreply.github.com>
2022-12-11 21:38:31 -0500

    Update ImageProcessing.h

--- src/ImageProcessing.h
+++ src/ImageProcessing.h
@@ -1,9 +1,19 @@
 #ifndef IMAGEPROCESSING_H
 #define IMAGEPROCESSING_H
 
-int getRGBArray(string filename, int* redArray, int* greenArray, int* blueArray);
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int getRGBArray(string filename, double* redArray, double* greenArray, double* blueArray);
+void getRGB(string filename, double* red, double* green, double* blue);
+/*
 double getRed(string filename);
 double getGreen(string filename);
 double getBlue(string filename);
+*/
 
 #endif
