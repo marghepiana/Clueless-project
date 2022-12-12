c1bd40e8f548488e84a0b6aa08cc7b0f3fa996cb
suhanimitra <113700563+suhanimitra@users.noreply.github.com>
2022-12-11 21:39:14 -0500

    Update isCompatible.h

--- src/isCompatible.h
+++ src/isCompatible.h
@@ -1,9 +1,13 @@
 #ifndef ISCOMPATIBLE_H
 #define ISCOMPATIBLE_H
 
+#include "Clothing.h"
+
+using namespace std;
+
 //so far only checks if someone's shirt and pant colors are compatible
 //would work with head, foot and jacket if given more time
 //(5 conditions with 11 diff colors is so many options!!)
-bool isCompatible(Clothing, Clothing);
+bool isCompatible(Clothing* c1, Clothing* c2);
 
 #endif
